e64ddfa0b6d0daa005ceb5433965bf23dc211499
Andrei Cojea <andreicojea@gmail.com>
2018-06-12 21:02:32 +0300

    added login/logout e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,57 @@
 import { AppPage } from './app.po';
+import { browser, by, element, Key, ExpectedConditions as EC } from 'protractor';
 
 describe('project2 App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeAll(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should fail to login with non existing user', async () => {
+    await page.loginButton.click();
+    expect(page.loginModalTitle.getText()).toBe('Login');
+
+    await page.loginEmailField.clear();
+    await page.loginEmailField.sendKeys('nouser@noemail.com');
+
+    await page.loginPasswordField.clear();
+    await page.loginPasswordField.sendKeys('zzz');
+
+    await page.loginSubmit.click();
+    expect(page.loginErrorAlert.isPresent()).toBe(true);
+
+    await page.modalContainer.click();
+  });
+
+  it('should login with existing user', async () => {
+    await page.loginButton.click();
+    expect(page.loginModalTitle.getText()).toBe('Login');
+
+    await page.loginEmailField.clear();
+    await page.loginEmailField.sendKeys('test@yahoo.com');
+
+    await page.loginPasswordField.clear();
+    await page.loginPasswordField.sendKeys('parola');
+
+    await page.loginSubmit.click();
+
+    // wait for modal to close
+    await browser.wait(EC.not(EC.presenceOf(page.loginModal)));
+
+    expect(page.loginButton.isPresent()).toBe(false);
+    expect(page.addItemButton.isPresent()).toBe(true);
+    expect(page.userMenuToggle.isPresent()).toBe(true);
+    expect(page.userMenuToggle.getText()).toContain('Hi, te');
+  });
+
+  it('should logout', async () => {
+    await page.userMenuToggle.click();
+    await page.logoutButton.click();
+
+    expect(page.loginButton.isPresent()).toBe(true);
+    expect(page.addItemButton.isPresent()).toBe(false);
+    expect(page.userMenuToggle.isPresent()).toBe(false);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,24 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
+  navbar = element(by.tagName('app-navbar'));
+  loginButton = this.navbar.element(by.buttonText('Login'));
+  addItemButton = this.navbar.element(by.css('.add-item-button'));
+  userMenuToggle = this.navbar.element(by.css('.user.dropdown-toggle'));
+  userMenuDropdown = this.navbar.element(by.css('.dropdown-menu'));
+  logoutButton = this.userMenuDropdown.element(by.linkText('Logout'));
+
+  modalContainer = element(by.tagName('modal-container'));
+  loginModal = element(by.tagName('app-login-modal'));
+  loginModalTitle = this.loginModal.element(by.tagName('h5'));
+  loginEmailField = this.loginModal.element(by.css('input[type=email]'));
+  loginPasswordField = this.loginModal.element(by.css('input[type=password]'));
+  loginErrorAlert = this.loginModal.element(by.css('.alert-danger'));
+  loginSubmit = this.loginModal.element(by.css('button[type=submit]'));
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
 }
